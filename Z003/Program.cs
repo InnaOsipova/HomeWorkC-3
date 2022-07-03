142e692f9723a6b5f6693ad17d1d4120e4ff4699
Inna Osipova <rutheva_inna@mail.ru>
2022-07-03 13:04:20 +0300

    Выдает кубы числа

---
+++ Z003/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubeNum (int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write(Math.Pow (i, 3)+"; ");
+    }
+}
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number+"  -> ");
+CubeNum(number);
