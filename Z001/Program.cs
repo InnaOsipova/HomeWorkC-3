f8b14b592cd2fe921987c1f1af6a4ae202bf6aca
Inna Osipova <rutheva_inna@mail.ru>
2022-07-03 11:59:42 +0300

    Палиндром

---
+++ Z001/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string TransformationArray (string str)
+{
+    char [] array = str.ToCharArray();
+    Array.Reverse(array);
+    string fin = new string(array);
+    return fin;
+}
+
+Console.Write("Ведите пятизначное числоа : ");
+string number = Console.ReadLine();
+string newNumber = TransformationArray(number);
+
+if (newNumber != number)
+{
+    Console.WriteLine("Данная запись не является палиндромом");
+}
+else
+{
+    Console.WriteLine("Данная запись является палиндромом");
+}
